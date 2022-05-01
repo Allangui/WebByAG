32d16018355667dec339a2522ff3ba59abec4a51
Allangui <deenv69@hotmail.be>
2022-05-01 12:01:34 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   build:{
     chunkSizeWarningLimit:2000,
-    // rollupOptions: {
-    //   output:{
-    //       manualChunks(id) {
-    //         if (id.includes('node_modules')) {
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
               
-    //             return id.toString().split('node_modules/')[1].split('/')[0].toString();
-    //         }
-    //     }
-    //   }
-    // }
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
+      }
+    }
   }
 })
